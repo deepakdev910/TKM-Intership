6cb53dcfcc747df9e649e821e1aaa2c2385af063
deepakdev910 <deepsdeeps70@gmail.com>
2026-06-09 15:36:42 +0530

    Update Ripplecarryadder.v

--- Day1/Assignment/ripplecarryadder/Ripplecarryadder.v
+++ Day1/Assignment/ripplecarryadder/Ripplecarryadder.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/09/2026 12:13:25 PM
+// Design Name: 
+// Module Name: riplecarry
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module fuladder (
     input a,
     input b,
@@ -21,4 +43,3 @@ module riplecarry (
     fuladder fa2 (.a(A[2]), .b(B[2]), .cin(c2),  .sum(S[2]), .cout(c3));
     fuladder fa3 (.a(A[3]), .b(B[3]), .cin(c3),  .sum(S[3]), .cout(Cout));
 endmodule
-

922de858513ac93417b8ba23befe2125ce61155a
deepakdev910 <deepsdeeps70@gmail.com>
2026-06-09 15:38:25 +0530

    Update fulladder.v

--- Day1/Assignment/ripplecarryadder/fulladder.v
+++ Day1/Assignment/ripplecarryadder/fulladder.v
@@ -1,3 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06/09/2026 12:13:25 PM
+// Design Name: 
+// Module Name: riplecarry
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module fuladder (
     input a,
     input b,

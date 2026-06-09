998a9e111497fcb946c2741f7e5c026d045161c5
deepakdev910 <deepsdeeps70@gmail.com>
2026-06-09 15:21:04 +0530

    Create fulladder.v

---
+++ Day1/Assignment/ripplecarryadder/fulladder.v
@@ -0,0 +1,10 @@
+module fuladder (
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+    assign sum  = a ^ b ^ cin;
+    assign cout = (a & b) | (b & cin) | (cin & a);
+endmodule
